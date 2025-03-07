f4767d93ce81a38a49f7296ec13d0f7ee2a45431
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:25:33 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,1 +1,9 @@
+abstract class Shape {
+    protected double dim_one, dim_two;
+    protected int num_sides;
 
+    // Default constructor
+    public Shape() {
+        this.dim_one = this.dim_two = 0;
+        this.num_sides = 0;
+    }
