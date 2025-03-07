02b121c8b85175fc39c8890afe534ae1d2f059c9
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:41:29 +0530

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,15 +1,20 @@
-class Square extends Shape {
-    public Square(double side) {
-        super(side, side, 4);
+class Sphere extends Shape {
+    public Sphere(double radius) {
+        super(radius, 0, 0);
     }
 
     @Override
     public double calculateArea() {
-        return dim_one * dim_one;
+        return 4 * Math.PI * dim_one * dim_one;
     }
 
     @Override
     public double calculatePerimeter() {
-        return 4 * dim_one;
+        return 0; // No perimeter for a sphere
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
     }
 }
