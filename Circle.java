10b7f4514e956b2319c929a3d09dc626d9ebe3c4
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:29:34 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,1 +1,16 @@
 
+class Circle extends Shape {
+    public Circle(double radius) {
+        super(radius, 0, 0); // Only radius is needed
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one;
+    }
+}
