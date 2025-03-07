25ad4fa00a5dfd5172be23387305fa35848fa07a
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:40:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,3 +14,28 @@ class Main {
             System.out.println("1. Rectangle\n2. Square\n3. Circle\n4. Sphere\n5. Cylinder\n6. Equilateral Pyramid\n7. Exit");
             System.out.print("Enter your choice: ");
             choice = scan.nextInt();
+
+            
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter length: ");
+                    double length = scan.nextDouble();
+                    System.out.print("Enter width: ");
+                    double width = scan.nextDouble();
+                    Shape rectangle = new Rectangle(length, width);
+                    displayShapeDetails(rectangle);
+                    break;
+
+                case 2:
+                    System.out.print("Enter side of Square: ");
+                    double side = scan.nextDouble();
+                    Shape square = new Square(side);
+                    displayShapeDetails(square);
+                    break;
+
+                case 3:
+                    System.out.print("Enter radius of Circle: ");
+                    double radius = scan.nextDouble();
+                    Shape circle = new Circle(radius);
+                    displayShapeDetails(circle);
+                    break;
