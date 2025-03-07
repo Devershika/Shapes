685252f57a0a125d64573ff33384904ddd5c0cea
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:22:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
 //DEVERSHIKA MOHANE
 //23070126032
 //AIML A2
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\nSelect Shape:");
+            System.out.println("1. Rectangle\n2. Square\n3. Circle\n4. Sphere\n5. Cylinder\n6. Equilateral Pyramid\n7. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scan.nextInt();
