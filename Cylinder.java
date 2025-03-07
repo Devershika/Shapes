9e2678a5a32607aa796a39aaf4f779e36eb3a9d2
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:34:00 +0530

    Update Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,1 +1,13 @@
+class Cylinder extends Shape {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius, 0, 0);
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + height);
+    }
 
