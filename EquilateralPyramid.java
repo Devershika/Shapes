63e80e3e1127e5a483904a587f4fec4d7164dbf0
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:39:57 +0530

    Update EquilateralPyramid.java

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,1 +1,15 @@
 
+class EquilateralPyramid extends Shape {
+    private double height;
+
+    public EquilateralPyramid(double side, double height) {
+        super(side, side, 4);
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        double baseArea = dim_one * dim_one;
+        double lateralArea = 2 * dim_one * Math.sqrt((dim_one / 2) * (dim_one / 2) + height * height);
+        return baseArea + lateralArea;
+    }
