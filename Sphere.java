edbfa8f1f5ea8c6c6b83d993ab8b3b8bb6b06dc4
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-07 22:30:49 +0530

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,10 @@
+class Square extends Shape {
+    public Square(double side) {
+        super(side, side, 4);
+    }
+
+    @Override
+    public double calculateArea() {
+        return dim_one * dim_one;
+    }
 
